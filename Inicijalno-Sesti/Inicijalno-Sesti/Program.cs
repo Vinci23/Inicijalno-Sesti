cb54be9a7aecaa0535f6891a549686631dbf95b6
Vinci23 <88689746+Vinci23@users.noreply.github.com>
2022-10-16 23:44:52 +0200

    Add files via upload

---
+++ Inicijalno-Sesti/Inicijalno-Sesti/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inicijalno_Sesti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {           
+                int brClanova;
+                double suma = 0;
+                double br = 1;
+
+                Console.WriteLine("Unesi broj članova reda: ");
+                brClanova = Convert.ToInt32(Console.ReadLine());
+
+                for (int i=0; i<brClanova; i++)
+                {
+                    br = br*3;
+                    suma = suma+(1/br);
+                }
+
+                Console.WriteLine(suma);
+                Console.ReadKey();
+            
+        }
+    }
+}
+
